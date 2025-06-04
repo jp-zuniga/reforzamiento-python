b05a2da870b039cede4434bf02378603b8b6b87d
jp-zuniga <jperez070705@gmail.com>
2025-06-04 09:02:06 -0600

    inicio de sesion-4

---
+++ sesion-4/models/autor.py
@@ -0,0 +1,9 @@
+"""
+Implementación de la clase Autor.
+"""
+
+
+class Autor:
+    def __init__(self, nombre, apellido):
+        self.nombre = nombre
+        self.apellido = apellido
---
+++ sesion-4/views/menu.py
@@ -0,0 +1,37 @@
+"""
+Implementación de la interfaz del sistema.
+"""
+
+
+from .inputs import Inputs
+import os
+
+
+def menu():
+    inputs: Inputs = Inputs()
+    while True:
+        print("=== Sistema de Gestión de Biblioteca ===")
+        print("1. Agregar Libro")
+        print("2. Mostrar Todos los Libros")
+        print("3. Buscar Libro por Titulo o ISBN")
+        print("4. Eliminar Libro por ISBN")
+        print("5. Salir")
+        opc = input("Selecione una opción: ")
+        match opc:
+            case "1":
+                inputs.case_one()
+                os.system("pause")
+            case "2":
+                inputs.case_two()
+                os.system("pause")
+            case "3":
+                inputs.case_three()
+                os.system("pause")
+            case "4":
+                inputs.case_four()
+                os.system("pause")
+            case "5":
+                print("Saliendo...")
+                break
+            case _:
+                print("Opcion invalida...")
